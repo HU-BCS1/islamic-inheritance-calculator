6088399527a6d5503989ac122071464fb294e93f
ducaale <sharaf.13@hotmail.com>
2019-05-15 11:16:35 +0300

    check input

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,10 @@ import { calculateTasib } from './tasib'
 import { calculateSpecialCases } from './specialCases'
 
 const calculate = (heirs: Partial<Heirs>) => {
+  if(heirs.husband && heirs.wife) {
+    throw Error('heirs cannot contain both husband and wife')
+  }
+
   const allHeirs: Heirs = { ...defaultHeirs, ...heirs }
   const fardResult = calculateFard(allHeirs)
   const tasibResult = calculateTasib(allHeirs, fardResult)
