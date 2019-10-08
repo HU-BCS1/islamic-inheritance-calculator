028dc468288951330b16f016bb579d93d101c095
ducaale <sharaf.13@hotmail.com>
2019-10-08 22:38:16 +0100

    add more tests

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -24,11 +24,6 @@ function checkResult(
 const f = (num: number, den: number = 1) => new Fraction(num, den)
 
 describe('Some edge cases', () => {
-  test('empty input', () => {
-    const result = calculate({})
-    expect(result.length).toEqual(0)
-  })
-
   test('single spouse', () => {
     calculate({ wife: 1 })
     calculate({ husband: 1 })
@@ -246,6 +241,96 @@ test('1 wife, 1 daughter, mother, 1 full_uncle', () => {
   checkResult(result, 'daughter', f(1,2))
   checkResult(result, 'mother', f(1,6))
   checkResult(result, 'full_paternal_uncle', f(5,24))
+})
+
+test('1 wife, 2 son, mother, full_uncle', () => {
+  const result = calculate({ wife: 1, son: 2, mother: 1, full_paternal_uncle: 1 })
+  checkResult(result, 'wife', f(1,8))
+  checkResult(result, 'son', f(17,24))
+  checkResult(result, 'mother', f(1,6))
+})
+
+test('1 wife, 1 son, 1 daughter, 1 mother, 1 full_uncle', () => {
+  const result = calculate({
+    wife: 1,
+    son: 1,
+    daughter: 1,
+    mother: 1,
+    full_paternal_uncle: 1
+  })
+
+  checkResult(result, 'wife', f(1,8))
+  checkResult(result, 'son', f(17,36))
+  checkResult(result, 'daughter', f(17,72))
+  checkResult(result, 'mother', f(1,6))
+})
+
+test('1 wife, 2 daughter, mother, full_uncle', () => {
+  const result = calculate({
+    wife: 1,
+    daughter: 2,
+    mother: 1,
+    full_paternal_uncle: 1
+  })
+
+  checkResult(result, 'wife', f(1,8))
+  checkResult(result, 'daughter', f(2,3))
+  checkResult(result, 'mother', f(1,6))
+  checkResult(result, 'full_paternal_uncle', f(1,24))
+})
+
+test('empty input', () => {
+  const result = calculate({})
+  expect(result.length).toEqual(0)
+})
+
+test('husband, 1 son', () => {
+  const result = calculate({ husband: 1, son: 1 })
+  checkResult(result, 'husband', f(1,4))
+  checkResult(result, 'son', f(3,4))
+})
+
+test('husband, 1 son, 1 daughter', () => {
+  const result = calculate({ husband: 1, son: 1, daughter: 1 })
+  checkResult(result, 'husband', f(1,4))
+  checkResult(result, 'son', f(1,2))
+  checkResult(result, 'daughter', f(1,4))
+})
+
+test('husband, 1 full_brother, 1 full_sister', () => {
+  const result = calculate({ husband: 1, full_brother: 1, full_sister: 1 })
+  checkResult(result, 'husband', f(1,2))
+  checkResult(result, 'full_brother', f(1,3))
+  checkResult(result, 'full_sister', f(1,6))
+})
+
+test('2 daughter, father, mother', () => {
+  const result = calculate({ daughter: 2, father: 1, mother: 1 })
+  checkResult(result, 'daughter', f(2,3))
+  checkResult(result, 'father', f(1,6))
+  checkResult(result, 'mother', f(1,6))
+})
+
+test('2 daughter, 2 maternal_sibling', () => {
+  const result = calculate({ daughter: 2, maternal_sibling: 2})
+  checkResult(result, 'daughter', f(1))
+})
+
+test('3 son', () => {
+  const result = calculate({ son: 3 })
+  checkResult(result, 'son', f(1))
+})
+
+test('1 son, 2 daughter', () => {
+  const result = calculate({ son: 1, daughter: 2 })
+  checkResult(result, 'son', f(1,2))
+  checkResult(result, 'daughter', f(1,2))
+})
+
+test('2 full_brother, 1 full_sister', () => {
+  const result = calculate({ full_brother: 2, full_sister: 1 })
+  checkResult(result, 'full_brother', f(4,5))
+  checkResult(result, 'full_sister', f(1,5))
 })
 
 test('TEMP: 1 wife, 1 daughter, 1 grand_daughter, mother', () => {
